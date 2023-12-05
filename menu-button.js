f9d740069d8d9f66171c3f81010394bc73aad81b
Richard Maher <maherrj@gmail.com>
2023-12-05 09:57:07 +0800

    First upload

---
+++ menu-button.js
@@ -0,0 +1,93 @@
+import sheet from '/menu-button.css' assert {type: 'css'};
+
+export class ABCMenuButton extends HTMLElement 
+{
+	#shadowRoot;
+	#button;
+	
+    constructor() {
+        super();
+
+        this.#shadowRoot = this.attachShadow({ mode: 'open' });
+		this.#shadowRoot.adoptedStyleSheets = [sheet];		
+		this.#button = document.createElement('div');
+		this.#button.classList.add("menu_btn");
+		this.#button.dataset["clicked"] = false;
+		this.#button.appendChild(document.createElement("div"));
+        let container = document.createElement('div'); 
+		container.classList.add('container');
+		container.appendChild(this.#button);
+		
+		this.#shadowRoot.appendChild(container);
+		let parentStyle = getComputedStyle(this.#shadowRoot.host);
+		this.#shadowRoot.host.style.setProperty('--default-bg-color', parentStyle.backgroundColor);
+		this.#shadowRoot.host.style.setProperty('--default-fg-color', parentStyle.color);
+		
+        if (this.hasAttribute("clicked")) 
+		{
+			let clicked = this.getAttribute("clicked");
+			this.#button.dataset.clicked = (String(clicked).toLowerCase() === "true");
+		}
+		else
+		{
+			this.setAttribute("clicked", "false");
+			this.#button.dataset.clicked = false;
+		}
+
+    }
+
+    static get observedAttributes() {
+        return ['clicked'];
+    }
+
+    connectedCallback() {
+
+        if (!this.isConnected) return;
+		
+		this.#button.addEventListener("click", this.#menuClicked)
+    }
+
+    disconnectedCallback() 
+	{
+		this.#button.removeEventListener("click", this.menuClicked)
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+		console.log("Attribute changed " + name);
+        if (!this.isConnected) return;
+
+        if (newValue == null) {
+            this[name] = "";
+            delete this[name];
+            return;
+        }
+	}
+
+	#menuClicked(e) {
+		let menu = e.target.classList.contains("menu_btn") ? e.target : e.target.closest(".menu_btn");
+
+		menu.addEventListener("animationend", (e) => {
+							menu.classList.remove('menuClick');
+						} , {"once": true}
+					);
+		if (menu.dataset["clicked"] != undefined) {
+			menu.addEventListener("animationend", 
+				(e) => {
+					   menu.classList.remove('menuClick');
+					   } , {"once": true}
+					   );
+			menu.classList.add('menuClick');
+			menu.dataset.clicked = (menu.dataset.clicked == "false") ? true : false;
+			this.setAttribute("clicked", menu.dataset.clicked);
+		}
+	}
+
+    get clicked() {
+        return this.#button.dataset.clicked;
+    }
+    set clicked(newValue) {
+		this.#button.dataset.clicked = (String(newValue).toLowerCase() === "true");
+        this.setAttribute("clicked", this.#button.dataset.clicked);
+    }
+}
+customElements.define('abc-menu-button', ABCMenuButton);
