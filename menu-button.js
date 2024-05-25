4b2b6420fc62c7fb9bb4068f648010f93ce04093
Richard Maher <maherrj@gmail.com>
2024-05-25 15:04:27 +0800

    Latest Version
    Added Disabled and Pressed attributes and Aria support

--- menu-button.js
+++ menu-button.js
@@ -1,123 +1,134 @@
-import sheet from './menu-button.css' assert {type: 'css'};
-
 export class ABCMenuButton extends HTMLElement 
 {
-	#shadowRoot;
 	#button;
-	#id;
+	#internals;
 	
     constructor() {
         super();
 
-        this.#shadowRoot = this.attachShadow({ mode: 'open' });
-		this.#shadowRoot.adoptedStyleSheets = [sheet];		
+        this.attachShadow({ mode: 'open' });
+		this.#internals = this.attachInternals();
+		this.#internals.ariaRole = "menu";
+		
+		let link = document.createElement('link');
+		link.href = "./menu-button.css";
+		link.rel = "stylesheet";
+		link.type = "text/css";
+		this.shadowRoot.appendChild(link);
+				
 		this.#button = document.createElement('div');
 		this.#button.classList.add("menu_btn");
-		this.#button.dataset["clicked"] = false;
+		this.#button.dataset["pressed"] = false;
 		this.#button.appendChild(document.createElement("div"));
+		
         let container = document.createElement('div'); 
 		container.classList.add('container');
 		container.appendChild(this.#button);
 		
-		this.#shadowRoot.appendChild(container);
+		this.shadowRoot.appendChild(container);
+		let parentStyle = getComputedStyle(this.shadowRoot.host);
+		this.shadowRoot.host.style.setProperty('--default-bg-color', parentStyle.backgroundColor);
+		this.shadowRoot.host.style.setProperty('--default-fg-color', parentStyle.color);
+				
+		this.setAttribute("aria-disabled", this.hasAttribute("disabled"));
 		
-        if (this.hasAttribute("clicked")) 
-		{
-			let clicked = this.getAttribute("clicked");
-			this.#button.dataset.clicked = (String(clicked).toLowerCase() === "true");
-		}
-		else
-		{
-			this.setAttribute("clicked", "false");
-			this.#button.dataset.clicked = false;
-		}
+		this.#button.dataset.pressed = this.hasAttribute("pressed");
 		
-        if (this.hasAttribute("id")) 
-		{
-			this.#id = this.getAttribute("id");
-		}
-		else
-		{
-			console.warn("abc-menu-button, cannot default colors without ID attribute");
-		}
-
+		this.setAttribute("aria-pressed", this.#button.dataset.pressed);
+		
+		this.#button.addEventListener("click", (e) => {
+			let menu = e.target.classList.contains("menu_btn") ? e.target : e.target.closest(".menu_btn");
+			if (this.hasAttribute("disabled")) {
+				e.stopPropagation();
+				e.preventDefault();
+				return;
+			}
+			
+			if (menu.dataset["pressed"] != undefined) {
+				menu.addEventListener("animationend", 
+					(e) => {
+						   menu.classList.remove('menuClick');
+						   }, {"once": true}
+						   );
+				menu.classList.add('menuClick');
+				if (menu.dataset.pressed == "false") {
+					this.setAttribute("pressed", "");
+				} else {
+					this.removeAttribute("pressed");
+				}
+			}
+		});
     }
 
     static get observedAttributes() {
-        return ['clicked'];
+        return ['pressed', 'disabled'];
     }
 
     connectedCallback() {
 
         if (!this.isConnected) return;
 		
-		this.#button.addEventListener("click", this.#menuClicked)
-		this.addEventListener("click", (e) => {
-				e.target.clicked = e.target.clicked == "true" ? "false" : "true";
-			});
-			
-		if (this.#id != null)
-		{
-			setTimeout(() => {
-				let lightMe = document.getElementById(this.#id);
-				let parentStyle = getComputedStyle(lightMe.parentNode);
-				this.#shadowRoot.host.style.setProperty('--default-fg-color', parentStyle.color);
-				this.#shadowRoot.host.style.setProperty('--default-bg-color', this.#findBackgroundColor(lightMe.parentNode));
-			}, 0);
-		}
+		let parentStyle = getComputedStyle(this.shadowRoot.host);
+		
+		if (parentStyle.backgroundColor.replace(/ /g,"") != "rgba(0,0,0,0)")
+			this.shadowRoot.host.style.setProperty('--default-bg-color', parentStyle.backgroundColor);
+		
+		this.shadowRoot.host.style.setProperty('--default-fg-color', parentStyle.color);
     }
 
     disconnectedCallback() 
 	{
-		this.#button.removeEventListener("click", this.menuClicked)
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-		console.log("Attribute changed " + name);
         if (!this.isConnected) return;
+		
+		console.log("Change " + name + " value " + newValue);
 
-        if (newValue == null) {
-            this[name] = "";
-            delete this[name];
-            return;
-        }
-	}
-
-	#menuClicked(e) {
-		let menu = e.target.classList.contains("menu_btn") ? e.target : e.target.closest(".menu_btn");
-
-		if (menu.dataset["clicked"] != undefined) {
-			menu.addEventListener("animationend", 
-				(e) => {
-					   menu.classList.remove('menuClick');
-					   } , {"once": true}
-					   );
-			menu.classList.add('menuClick');
+		switch	(name) {
+			case	"pressed":
+				if (this.hasAttribute(name)) {
+					this.setAttribute("aria-"+name, "true");
+					this.#button.dataset[name] = true;
+				} else {
+					this.#button.dataset[name] = false;
+					this.setAttribute("aria-"+name, "false");
+				}
+				break;
+			case	"disabled":
+				if (this.hasAttribute(name)) {
+					this.setAttribute("aria-"+name, "true");
+				} else {
+					this.setAttribute("aria-"+name, "false");
+				}
+				break;
+			default:
+				throw new Error("Unknown attribute modified " + name);
 		}
 	}
-	
-	#findBackgroundColor(elem) {
-			let parentStyle = getComputedStyle(elem);
-			if (parentStyle.backgroundColor != "rgba(0, 0, 0, 0)") 
-			{
-				return parentStyle.backgroundColor;
-			}
-			else {
-				if (elem.tagName.toLowerCase() == "body") {
-					return elem.backgrounColor;
-				}
-				else {
-					return this.#findBackgroundColor(elem.parentNode);
-				}
-			}
-	}
 
-    get clicked() {
-        return this.#button.dataset.clicked;
+    get pressed() {
+        return this.hasAttribute("pressed");
     }
-    set clicked(newValue) {
-		this.#button.dataset.clicked = (String(newValue).toLowerCase() === "true");
-        this.setAttribute("clicked", this.#button.dataset.clicked);
+	
+    set pressed(newValue) {
+		if (String(newValue).toLowerCase() !== "true") {
+			this.removeAttribute("pressed")
+		} else {
+			this.setAttribute("pressed", "");
+		}
+    }
+	
+    get disabled() {
+        return this.hasAttribute("disabled");
+    }
+	
+    set disabled(newValue) {
+		if (String(newValue).toLowerCase() !== "true") {
+			this.removeAttribute("disabled")
+		} else {
+			this.setAttribute("disabled", "");
+		}
     }
 }
 customElements.define('abc-menu-button', ABCMenuButton);
